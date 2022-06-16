0cf2a265cc21f6290da4accf3f3ed06819288995
calfsama <tomirisnegmatova@gmail.com>
2022-06-16 22:31:09 +0500

    first commit

---
+++ Coupling/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coupling
+//
+//  Created by Tomiris Negmatova on 16/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
